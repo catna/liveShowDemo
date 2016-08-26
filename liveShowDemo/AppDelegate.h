ec1671ed12d3698f69603c7d0fdad6f7ea0d2771
catna <catnaer@163.com>
2016-08-26 11:07:46 +0800

    Init

---
+++ liveShowDemo/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  liveShowDemo
+//
+//  Created by MX on 16/8/26.
+//  Copyright © 2016年 mx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ liveShowDemo/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  liveShowDemo
+//
+//  Created by MX on 16/8/26.
+//  Copyright © 2016年 mx. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
